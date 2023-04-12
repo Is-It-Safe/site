29cd1014538705f73ec72e875617314d4e8ab9af
alevieiramoreira <alessandracvmoreira@gmail.com>
2023-04-12 02:18:16 -0300

    feat: adiciona mock de membros do time

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Header } from '../../components/Header/Header';
 import { SubsCriptionModal } from '../../components/SubscriptionModal/SubscriptionModal';
+import { teamMembers } from '../../mocks/team';
+import emptyCard from '../../assets/teamcardimg.png';
 
 import './Team.styles.css';
 
@@ -47,7 +49,19 @@ export const Team = () => {
 						fazer nosso projeto crescer.
 					</p>
 					<h2>Conheça nosso time:</h2>
-					<div className="team"></div>
+					<div className="team">
+						{teamMembers.map((member) => (
+							<div className="teamcard" key={member.name}>
+								<img
+									src={member.photo || emptyCard}
+									alt={member.name}
+								/>
+								<p className="pronomes">{member.pronouns}</p>
+								<p className="nome">{member.name}</p>
+								<p className="cargo">{member.role}</p>
+							</div>
+						))}
+					</div>
 				</section>
 			</main>
 		</>
