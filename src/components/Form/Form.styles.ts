40205a8644d835995ded05965522462f3329cda3
Hector-Santos <98065266+Hector-Santos@users.noreply.github.com>
2023-07-17 17:36:08 -0300

    fix/develop - correção de tipagem do formulario de inscrição

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+import { RefObject } from 'react';
+import { LegacyRef } from 'react';
+
+type Iform = {
+	ref: LegacyRef<HTMLFormElement> | ((instance: HTMLFormElement | null) => void) | RefObject<HTMLFormElement> | null | undefined;
+};
 
 export const FormContainer = styled.form`
 	width: 100%;
