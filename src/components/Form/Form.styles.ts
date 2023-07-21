784ec72b2bbe2cebe69d666b857519fa120c9e45
Hector-Santos <98065266+Hector-Santos@users.noreply.github.com>
2023-07-21 19:02:49 -0300

    feat/Politica de privacidade e termos e condições - merge main

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
-import { RefObject } from 'react';
-import { LegacyRef } from 'react';
 
-type Iform = {
-	ref: LegacyRef<HTMLFormElement> | ((instance: HTMLFormElement | null) => void) | RefObject<HTMLFormElement> | null | undefined;
-};
 
 export const FormContainer = styled.form`
 	width: 100%;
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -9,7 +9,7 @@ export type IForm =  React.DetailedHTMLProps<
 	handleSubmit: UseFormHandleSubmit<any>;
 	onSubmit: (data: any) => void;
 	children: ReactNode;
-	ref: ((instance: HTMLFormElement | null) => void) | RefObject<HTMLFormElement> | null | undefined;
+	ref?: ((instance: HTMLFormElement | null) => void) | RefObject<HTMLFormElement> | null | undefined;
 	
 };
 
