778fe78be2daa01c0ffbcd3e404f293c3ea66cef
Hector-Santos <98065266+Hector-Santos@users.noreply.github.com>
2023-07-30 20:58:03 -0300

    fix/responsiveness - menu mobile desaparece ao clicar fora

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import logoWhite from '../../assets/logo-white.png';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './Header.styles.css';
 
 
@@ -12,14 +12,24 @@ export const Header = ({ isMainPage }: HeaderProps) => {
 	const [mobileMenuDisplay, setMobileMenuDisplay] = useState<string>(
 		'none'
 	);
+	const menuRef = useRef<HTMLUListElement>(null);
+	const buttonRef = useRef<HTMLButtonElement>(null);
 
 	const closeMenu = () => {
 		setMobileMenuDisplay('none');
+		window.removeEventListener('click', handleClickOutside);
+	};
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (menuRef.current && buttonRef.current && !menuRef.current.contains(event.target as Node) && !buttonRef.current.contains(event.target as Node)){
+			closeMenu();	
+		}
 	};
 
 	const toggleMenu = () => {
 		if (mobileMenuDisplay === 'none') {
 			setMobileMenuDisplay('flex');
+			window.addEventListener('click', handleClickOutside);
 		} else {
 			closeMenu();
 		}
@@ -36,12 +46,12 @@ export const Header = ({ isMainPage }: HeaderProps) => {
 						<a href="contact">contato</a>
 					</nav>
 					<nav className="topmenu__mobile__main">
-						<button className="btn-toggle" id="menu-toggle" onClick={toggleMenu}>
+						<button ref={buttonRef} className="btn-toggle" id="menu-toggle" onClick={toggleMenu}>
 							<div className="line"></div>
 							<div className="line"></div>
 							<div className="line"></div>
 						</button>
-						<ul className="topmenu__mobile__list" style={{ display: mobileMenuDisplay }} onBlur={closeMenu}>
+						<ul ref={menuRef} className="topmenu__mobile__list" style={{ display: mobileMenuDisplay }}>
 							<a href="about">sobre</a>
 							<a href="team">nossa equipe</a>
 							<a href="donations">doações</a>
@@ -70,16 +80,16 @@ export const Header = ({ isMainPage }: HeaderProps) => {
 								<img src={logoWhite} alt="Is it safe?" />
 							</a>
 						</div>
-						<button className="btn-toggle" id="menu-toggle" onClick={toggleMenu}>
+						<button ref={buttonRef} className="btn-toggle" id="menu-toggle" onClick={toggleMenu}>
 							<div className="line"></div>
 							<div className="line"></div>
 							<div className="line"></div>
 						</button>
 
 						<ul
+							ref={menuRef}
 							className="topmenu__mobile__list"
 							style={{ display: mobileMenuDisplay }}
-							onBlur={closeMenu}
 						>
 							<a href="about">sobre</a>
 							<a href="team">nossa equipe</a>
